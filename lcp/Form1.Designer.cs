5a8deb94c783bf5c5899ce62910f53a276c46a9a
John Hines II <johnhines@gormanrupp.com>
2018-05-04 08:57:16 -0400

    Initial commit

---
+++ lcp/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace lcp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.volumeMeter1 = new NAudio.Gui.VolumeMeter();
+            this.SuspendLayout();
+            // 
+            // volumeMeter1
+            // 
+            this.volumeMeter1.Amplitude = 0F;
+            this.volumeMeter1.Location = new System.Drawing.Point(12, 161);
+            this.volumeMeter1.MaxDb = 18F;
+            this.volumeMeter1.MinDb = -60F;
+            this.volumeMeter1.Name = "volumeMeter1";
+            this.volumeMeter1.Size = new System.Drawing.Size(272, 23);
+            this.volumeMeter1.TabIndex = 0;
+            this.volumeMeter1.Text = "volumeMeter1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 261);
+            this.Controls.Add(this.volumeMeter1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NAudio.Gui.VolumeMeter volumeMeter1;
+    }
+}
+
---
+++ lcp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSCore;
+using System.Device;
+using MadeInTheUSB.i2c;
+using MadeInTheUSB.GPIO;
+using MadeInTheUSB.WinUtil;
+using MadeInTheUSB.Components;
+
+namespace lcp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        public static void fooBar()
+        {
+            
+        }
+    }
+}
